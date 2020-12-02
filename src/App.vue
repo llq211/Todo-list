9ee4a831e43f80739e3e9dcabc1d275f4dc8c51f
llq211 <936217760@qq.com>
2020-12-02 17:49:16 +0800

    第一次提交，写了输入和删除功能，还未写选中后分类是否完成的功能和样式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Button msg="Welcome to your Todolist!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import Button from './components/Button.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    Button
   }
-};
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="out_box">
+    <input type="text" name="" v-model="name" @keyup.enter="submit" placeholder="请输入代办事情">
+    <button class="click" @click="submit">提交</button> 
+    <ul>
+      <li v-for="(item,index) in list" :key="index">
+        <div class="box_todolist">
+          <span>{{index+1}}、</span>
+          <span>{{item}}</span>
+          <button class="Delete" @click="remove(index)"></button>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  
+</template>
+
+<script>
+export default{
+  name: "Button",
+  data(){
+    return{
+      name: "",
+      list: []
+    }
+  },
+  methods: {
+    submit: function(){
+      this.list.push(this.name);
+      this.name="";
+    },
+    remove: function(index) {
+      this.list.splice(index,1);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
